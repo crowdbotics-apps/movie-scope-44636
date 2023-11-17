25c9f7cbe2ab0c4fad3caee305d1f2a6250622b3
Crowdbotics <team@crowdbotics.com>
2023-11-17 03:58:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "movie-scope-44636",
+  url: "https://movie-scope-44636.botics.co",
+  api: "https://movie-scope-44636.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
